8d5e0e5e05b988ed8702a601fffa81ece0f535ed
roschek <roschek@yandex.ru>
2020-05-12 17:53:35 +0300

    zx

--- script/script.js
+++ script/script.js
@@ -2,7 +2,6 @@ const button = document.querySelector('.button');
 const buttonEdit = document.querySelector('.button__edit');
 const placesList = document.querySelector('.places-list');
 //++ Переменная не используется
-
 const popup = document.querySelector('.popup');
 const popupEdit = document.querySelector('.popup__edit');
 const popupPhoto = document.querySelector('.popup__photo');
@@ -21,21 +20,26 @@ const errorMessages = {
 
 function handlerInputForm(evt) {
   const submit = evt.currentTarget.querySelector('.button');
+ 
   // Надо исправить
-  //+++ Здесь тоже не только инпуты, но и кнопка
-  if(evt.currentTarget.elements!== submit ){
-  const [...inputs] = evt.currentTarget.elements;
+  // Здесь тоже не только инпуты, но и кнопка
+  // Не так поняли
   
-  isFieldValid(evt.target);
+    //+++ В этом массиве инпуты и кнопка
+    // Сюда надо только инпуты добавить из формы
+    const [...inputs] = evt.currentTarget.querySelectorAll('input')
+     
+    isFieldValid(evt.target);
+    
 
-  if (inputs.every(isValidate)) {
-    setSubmitButtonState(submit, true);
-  } else {
-    setSubmitButtonState(submit, false);
+    if (inputs.every(isValidate)) {
+      setSubmitButtonState(submit, true);
+    } else {
+      setSubmitButtonState(submit, false);
 
+    }
   }
-  }
-}
+
 
 // функция проверки
 function isValidate(input) {
@@ -64,27 +68,10 @@ function isValidate(input) {
 
 // вкл/выкл ошибки со слушателя
 function isFieldValid(input) {
-  // Надо исправить
-  // parentNode -- это хардкод, сломается сразу при любом изменении верстки
-  // Лучше, например, через closest найти форму и в ней найти уже этот элемент по селектору
-  const errorElem = input.parentNode.querySelector(`#${input.id}-error`);
+  const errorForm = input.closest('form');
+  const errorElem = errorForm.querySelector(`#${input.id}-error`);
   const valid = isValidate(input); // устанавливаем инпуту кастомные ошибки, если они есть.
   errorElem.textContent = input.validationMessage;
-  
-  return valid;
-}
-
- //проверки формы на валидность
- 
-function isFormValid() { // validateForm
-  const inputs = document.querySelectorAll('input');
-
-  let valid = true;
-   
-  inputs.forEach((input) => {
-    if (!isFieldValid(input)) valid = false;
-    
-  });
   
   return valid;
 }
@@ -105,32 +92,40 @@ function togglePopup() {
   popup.classList.toggle('popup_is-opened');
   submitForm.setAttribute('disabled', true);
   form.reset();
+  cleanSpan();
 };
+
 function togglePopupEdit() {
   popupEdit.classList.toggle('popup_is-opened');
   submitFormEdit.setAttribute('disabled', true);
+  cleanSpan()
   formEdit.reset();
 }
 
 function togglePopupPhoto() {
   popupPhoto.classList.toggle('popup_is-opened');
 }
+//очищаем поля спанам
+function cleanSpan() {
+  const collector = document.querySelectorAll('span')
+  collector.forEach( function(element){
+    element.textContent = '';
+  })
+}
 
 // изменение названия карточки
-// Вот тут не соблюдается
-// ++ camelCase
 function changePerson(event) {
   event.preventDefault();
   const userName = document.querySelector('.user-info__name');
   const userDescription = document.querySelector('.user-info__job');
-  //++ Можно лучше
-  // Деструктуризация
   const { pers, description } = formEdit.elements
   userName.textContent = pers.value;
   userDescription.textContent = description.value;
   formEdit.reset();
-  togglePopupEdit()
+  togglePopupEdit();
+  
 }
+
 
 // создание карточки с фоткой
 
@@ -179,18 +174,15 @@ function addPhoto() {
     placesList.appendChild(photoArray);
   })
 }
-//+++/спасибо, разобрался, с for просто привычнее было, придется отвыкать/ Надо исправить
-// Вы меня простите, но на 7 спринте уже пора
-// использовать forEach без обращения к индексам
 
 // добавление карточек попапом
 
-function addNewPhoto() {
+function addNewPhoto(event) {
+  // event не передан
+  // надо исправить
   event.preventDefault();
   const { name, link } = form.elements;
-
   const cardContainer = createNewPhoto(name.value, link.value);
-
   placesList.appendChild(cardContainer);
   form.reset();
   togglePopup();
@@ -206,8 +198,6 @@ placesList.addEventListener('click', function (event) {
 //  удаление элемента
 
 placesList.addEventListener('click', function (event) {
-
-  //++ const!!!
   const currentCard = event.target;
 
   if (currentCard.classList.contains('place-card__delete-icon')) {
@@ -219,15 +209,11 @@ placesList.addEventListener('click', function (event) {
 
 // eslint-disable-next-line prefer-arrow-callback
 placesList.addEventListener('click', function (event) {
-  //++ const
   const currentCard = event.target;
   const popupImage = document.querySelector('.popup__image');
   if (currentCard.classList.contains('place-card__image')) {
-    //++ Не надо длинного условия, проверьте -- в картинку попали?
-    //++ Супер, оставливаем всплытие события и выполняем все что вот дальше.
     const image = currentCard.getAttribute('data-image')
-    console.log(image)
-    popupImage.setAttribute('src', image);
+     popupImage.setAttribute('src', image);
 
     togglePopupPhoto();
   }
@@ -237,44 +223,23 @@ form.addEventListener('input', handlerInputForm, true);
 formEdit.addEventListener('input', handlerInputForm, true);
 buttonEdit.addEventListener('click', togglePopupEdit);
 button.addEventListener('click', togglePopup);
-popupClose.addEventListener('click', togglePopup);
-popupCloseEdit.addEventListener('click', togglePopupEdit);
-popupClosePhoto.addEventListener('click', togglePopupPhoto);
+popupClose.addEventListener('click', togglePopup,);
+popupCloseEdit.addEventListener('click', togglePopupEdit,);
+popupClosePhoto.addEventListener('click', togglePopupPhoto, );
+
 form.addEventListener('submit', addNewPhoto);
 formEdit.addEventListener('submit', changePerson);
 addPhoto();
 
-// Работа отклоняется от проверки
-// ++Код (за исключением массива с данными) должен быть в одном файле,
-// модули вы еще не изучали./исправил но странно, в задании было сказано 'Если чувствуете, что скрипт разрастается — разбейте его на отдельные файлы и подключите их все в index.html.' /
-// ++Зум реализован некорректно, пропорции исходного изображения искажаются.// исправил
-// посмотрел+++/Вроде везде camelCase несколько раз п/ Не везде соблюден camelCase
-// ---
 
-// Советы (без учета которых сдача работы будет затруднительна):
-// +++/исправил, вроде работает/ Для того чтобы искть URL не надо делать сложного парсинга. При создании карточки добавьте ей атрибут с адресом.
-// А когда вам он понадобится -- прочитаете. Как это сделать: https://developer.mozilla.org/ru/docs/Web/HTML/Global_attributes/data-*
-
-// ++  Чтобы получить все инпуты формы надо пользоваться не spread оператором, а querySelectorAll, например, и сразу выбрать
-// все инпуты из формы. Тогда не придется проверять кнопка это или нет каждый раз./спасибо, исправил/
+// Здравствуйте
 
 
+// В форму редактирования данных юзера должна подставляться актуальная информация, но не в плейсхолдеры,
+// а в инпуты.
 
-// Ревью 1
-//
-// Здравствуйте!
-//
-// У формы есть метод checkValidity() -- он вернет true или false таким образом задача кастомной валидации сводится
-// вот к чему:
+//++ Если вызвать ошибку валидации на инпуте (появится текст ошибки) и просто закрыть
+// крестиком форму, то тогда при ее повторном открытии остается залипший текст ошибок.
+// Этого быть не должно.
 
-// Создаете метод который будет устанавливать обработчики на переданную в него форму
-// Внутри этого метода один раз получите все инпуты формы и кнопку и передадите в обработчик, который устанавливаете
-// Далее тот обработчик вызывает на target события проверку поля, выводит или убирает ошибку, а потом
-// вызывается функция контроля кнопки которая на форме проверит checkValidity()
-
-// В том виде в котором сейчас это у вас -- это массо лишних обращений в DOM и код дублирующий функционал.
-// Реализуйте просто и чисто, простота вообще залог хорошего кода.
-
-//++ Крестик у попапа зума совсем не там где по макету.
-
-// Это и все комментарии надо исправить.
+// Это надо исправить.
