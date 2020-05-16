6e7f4ccb4f2d02162e16ab4a68305ab655ebc03a
Alexey Roschektaev <55943475+roschek@users.noreply.github.com>
2020-05-16 23:19:31 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -11,33 +11,14 @@ const submitForm = document.querySelector('.popup__button');
 const submitFormEdit = document.querySelector('.button__popup-edit');
 const form = document.forms.new;
 const formEdit = document.forms.person;
+const collector = document.querySelectorAll('.error')
 const errorMessages = {
   empty: 'Это обязательное поле',
   wrongLength: 'Должно быть от 2 до 30 символов',
   wrongUrl: 'Здесь должна быть ссылка',
 }
 
-function handlerInputForm(evt) {
-  const submit = evt.currentTarget.querySelector('.button');
- 
-  // Надо исправить
-  // Здесь тоже не только инпуты, но и кнопка
-  // Не так поняли
-  
-    //+++ В этом массиве инпуты и кнопка
-    // Сюда надо только инпуты добавить из формы
-    const [...inputs] = evt.currentTarget.querySelectorAll('input')
-     
-    isFieldValid(evt.target);
-    
 
-    if (inputs.every(isValidate)) {
-      setSubmitButtonState(submit, true);
-    } else {
-      setSubmitButtonState(submit, false);
-
-    }
-  }
 
 
 // функция проверки
@@ -51,9 +32,7 @@ function isValidate(input) {
   }
 
   if (input.validity.tooShort || input.validity.tooLong) {
-    //++ Вы же объект с ошибками завели, так им пользуйтесь!
     input.setCustomValidity(errorMessages.wrongLength);
-
     return false
   }
   if (input.validity.typeMismatch) {
@@ -65,15 +44,14 @@ function isValidate(input) {
   return input.checkValidity();
 }
 
+
 // вкл/выкл ошибки со слушателя
 function isFieldValid(input) {
-
   const errorForm = input.closest('form');
-
   const errorElem = errorForm.querySelector(`#${input.id}-error`);
   const valid = isValidate(input); // устанавливаем инпуту кастомные ошибки, если они есть.
   errorElem.textContent = input.validationMessage;
-  console.log(errorForm);
+
   return valid;
 }
 
@@ -82,58 +60,101 @@ function isFieldValid(input) {
 function setSubmitButtonState(button, state) {
   if (state) {
     button.removeAttribute('disabled');
-
+    button.classList.add(`popup__button_valid`);
+    button.classList.remove(`popup__button_invalid`)
   } else {
     button.setAttribute('disabled', true);
+    button.classList.add(`popup__button_invalid`);
+    button.classList.remove(`popup__button_valid`);
+  }
+}
+
+function handlerInputForm(inputs, submit) {
+  /*const submit = evt.currentTarget.querySelector('.button');
+  // Можно лучше
+  // spread оператор не нужен
+  const [...inputs] = evt.currentTarget.querySelectorAll('input')*/
+  /*isFieldValid(evt.target);*/
+  inputs.forEach(isFieldValid);
+
+  if (inputs.every(isValidate)) {
+
+    setSubmitButtonState(submit, true);
+  } else {
+    setSubmitButtonState(submit, false);
 
   }
+}
+
+// Этот метод должен вызваться один раз для каждой формы, которую вы хотите валидировать
+// Он не прикрепляется к событию
+function checkForm(form) {
+  // Кнопку получили, отлично
+  // Только currentTarget уберите, он и тут и у массива ни к чему совсем
+  const submit = form.querySelector('.button');
+  // Массив получили, но не пойму почему не простое присвоение а через [...]
+  const [...inputs] = form.querySelectorAll('input')
+ console.log(inputs,submit);
+  form.addEventListener('input',()=>handlerInputForm(inputs,submit))
+  // Теперь на полученную форму установите слушатель события input
+  // Обработчиком будет являться
+  // () => handlerInputForm(inputs, submit)
+  
+  // Таким образом форма будет слушать событие ввода, как только оно произойдет,
+  // обработчик пробежит по инпутам, проверит форму и кнопку приведет в соответствие.
+  // Этим методом вы легко прикрутите валидацию ко второй форме, его просто вызвать достаточно передав туда
+  // объект формы для карты.
+}
+
+// ++Можно лучше
+//++ Чистите ошибки а не спаны все же, имя лучше другое
+function cleanError() {
+  collector.forEach(function (element) {
+    element.textContent = '';
+  })
 }
 
 function togglePopup() {
   popup.classList.toggle('popup_is-opened');
   submitForm.setAttribute('disabled', true);
   form.reset();
-  cleanSpan();
+  cleanError();
 };
 
 function togglePopupEdit() {
   popupEdit.classList.toggle('popup_is-opened');
   submitFormEdit.setAttribute('disabled', true);
-  cleanSpan()
+  cleanError()
   formEdit.reset();
 }
 
 function togglePopupPhoto() {
   popupPhoto.classList.toggle('popup_is-opened');
 }
-//очищаем поля спанам
-function cleanSpan() {
-  const collector = document.querySelectorAll('span')
-  collector.forEach( function(element){
-    element.textContent = '';
-  })
-}
+
+
 
 // изменение названия карточки
-
 function changePerson(event) {
   event.preventDefault();
   const userName = document.querySelector('.user-info__name');
   const userDescription = document.querySelector('.user-info__job');
-  const { pers, description} = formEdit.elements
+  const { pers, description } = formEdit.elements
   userName.textContent = pers.value;
   userDescription.textContent = description.value;
   formEdit.reset();
   togglePopupEdit();
-  
+
 }
 
-function cardValue(){
+function cardValue() {
   const name = document.querySelector('.user-info__name').innerHTML;
   const profession = document.querySelector('.user-info__job').innerHTML;
-  const { pers, description} = formEdit.elements
-  pers.value=name;
-  description.value=profession;
+  const { pers, description } = formEdit.elements
+  pers.value = name;
+  description.value = profession;
+  submitFormEdit.removeAttribute('disabled');
+  submitFormEdit.classList.add(`popup__button_valid`);
 }
 
 // создание карточки с фоткой
@@ -178,10 +199,10 @@ function createNewPhoto(nameValue, linkValue) {
 //  добавление карточек при загрузке
 
 function addPhoto() {
-
+  // Можно лучше
+  // (elem) => {...}
   initialCards.forEach(function (elem) {
     const photoArray = createNewPhoto(elem.name, elem.link);
-
     placesList.appendChild(photoArray);
   })
 }
@@ -189,8 +210,6 @@ function addPhoto() {
 // добавление карточек попапом
 
 function addNewPhoto(event) {
-  // event не передан
-  // надо исправить
   event.preventDefault();
   const { name, link } = form.elements;
   const cardContainer = createNewPhoto(name.value, link.value);
@@ -209,9 +228,7 @@ placesList.addEventListener('click', function (event) {
 //  удаление элемента
 
 placesList.addEventListener('click', function (event) {
-
   const currentCard = event.target;
-
 
   if (currentCard.classList.contains('place-card__delete-icon')) {
     currentCard.closest('.place-card').remove();
@@ -220,31 +237,48 @@ placesList.addEventListener('click', function (event) {
 
 // открываем фотку
 
-// eslint-disable-next-line prefer-arrow-callback
 placesList.addEventListener('click', function (event) {
-
   const currentCard = event.target;
   const popupImage = document.querySelector('.popup__image');
   if (currentCard.classList.contains('place-card__image')) {
-
     const image = currentCard.getAttribute('data-image')
-     popupImage.setAttribute('src', image);
+    popupImage.setAttribute('src', image);
 
     togglePopupPhoto();
   }
 });
 
-form.addEventListener('input', handlerInputForm, true);
-formEdit.addEventListener('input', handlerInputForm, true);
+// Надо исправить
+// Заведите метод, который на вход получает форму.
+// Метод внутри себя ставит на нее обработчик input.
+// Обработчик выбирает из формы массив инпутов и кнопку, вызывает handlerInputForm и передает ему массив и кнопку.
+// Теперь при каждом нажатии клавиши не надо выбирать все инпуты из формы заново.
+// Вызовете этот метод для двух форм = профит!
+
+// спасибо, вроде начал немного понимать, хотя не уверен что правильно...
+
+// Пока неправильно. Этот метод не должен быть обработчиком. Он вызывается один раз с переданным в него параметром
+// Если надо другую форму валидировать, то вызовете его еще раз с другим параметром.
+
+//form.addEventListener('input', checkForm, true);
+//formEdit.addEventListener('click', checkForm, true);
+
 buttonEdit.addEventListener('click', togglePopupEdit);
-buttonEdit.addEventListener('click',cardValue );
+buttonEdit.addEventListener('click', cardValue);
 button.addEventListener('click', togglePopup);
-popupClose.addEventListener('click', togglePopup,);
-popupCloseEdit.addEventListener('click', togglePopupEdit,);
-popupClosePhoto.addEventListener('click', togglePopupPhoto, );
+popupClose.addEventListener('click', togglePopup);
+popupCloseEdit.addEventListener('click', togglePopupEdit);
+popupClosePhoto.addEventListener('click', togglePopupPhoto);
 
 form.addEventListener('submit', addNewPhoto);
 formEdit.addEventListener('submit', changePerson);
 addPhoto();
+checkForm(form);
+checkForm(formEdit);
 
+//ствуйте
 
+// Надо исправить
+// Валидация не работает сейчас. См. комментарии в коде.
+
+// Исправьте, и присылайте.
