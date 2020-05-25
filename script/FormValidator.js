ec4abd49bb8f746aceaa7f76efa9ee3226723ffa
Alexey Roschektaev <55943475+roschek@users.noreply.github.com>
2020-05-25 23:55:32 +0300

    Update FormValidator.js

--- script/FormValidator.js
+++ script/FormValidator.js
@@ -9,52 +9,51 @@ class FormValid {
     this.submit = this.form.querySelector('.button')
   }
 
-  isValidate (input) {
-    
-    input.setCustomValidity(""); 
+  isValidate(inputs) {
+ inputs.forEach((input)=>{
+    input.setCustomValidity("");
 
-      if (input.validity.valueMissing) {
-        input.setCustomValidity(this.errors.empty);
-        return false
-      }
-      if (input.value.trim().length === 0) {
-        input.setCustomValidity(this.errors.empty);
-        return false
-    
-      }
-      if (input.validity.tooShort || input.validity.tooLong) {
-        input.setCustomValidity(this.errors.wrongLength);
-        return false
-      }
-      if (input.validity.typeMismatch && input.type === 'url') {
-    
-        input.setCustomValidity(this.errors.wrongUrl);
-        return false
-      }
-    
-      input.nextElementSibling.textContent = "";
-    return true;
-    
-    
-  }
+    if (input.validity.valueMissing) {
+      input.setCustomValidity(this.errors.empty);
+      return false
+    }
+    if (input.value.trim().length === 0) {
+      input.setCustomValidity(this.errors.empty);
+      return false
+
+    }
+    if (input.validity.tooShort || input.validity.tooLong) {
+      input.setCustomValidity(this.errors.wrongLength);
+      return false
+    }
+    if (input.validity.typeMismatch && input.type === 'url') {
+
+      input.setCustomValidity(this.errors.wrongUrl);
+      return false
+    }
+
+    return input.checkValidity();
+
+
+  })}
 
 
   // чтобы делать кнопку сабмита активной и неактивной.
   setSubmitButtonState() {
-    
-      if (this.form.checkValidity() === true)  {
-        this.submit.removeAttribute('disabled', true);
-        this.submit.classList.add(`popup__button_valid`);
-        this.submit.classList.remove(`popup__button_invalid`);
-        
-      } else {
-        this.submit.setAttribute('disabled', true);
-        this.submit.classList.add(`popup__button_invalid`);
-        this.submit.classList.remove(`popup__button_valid`);
-        
-      }
+
+    if (this.form.checkValidity() === true) {
+      this.submit.removeAttribute('disabled', true);
+      this.submit.classList.add(`popup__button_valid`);
+      this.submit.classList.remove(`popup__button_invalid`);
+
+    } else {
+      this.submit.setAttribute('disabled', true);
+      this.submit.classList.add(`popup__button_invalid`);
+      this.submit.classList.remove(`popup__button_valid`);
+
     }
-  
+  }
+
 
 
   setEventListeners() {
@@ -64,33 +63,43 @@ class FormValid {
   }
 
   isFieldValid(input) {
-    const errorForm = input.form
+    
+    const errorForm = input
     const errorElem = errorForm.querySelector(`#${input.id}-error`);
     const valid = isValidate(input); // устанавливаем инпуту кастомные ошибки, если они есть.
     errorElem.textContent = input.validationMessage;
 
     return valid;
   }
-  
 
-  handlerInputForm(evt,submit) {
-    isFieldValid(this.form);
-  
-    const currentForm =evt.currentTarget;
-    if (currentForm.checkValidity()) {
-  
+  cleanError() {
+    collector.forEach(function (element) {
+      element.textContent = '';
+    })
+  }
+
+  handlerInputForm(submit) {
+    isFieldValid(this.formInputs);
+
+
+    if (this.form.checkValidity()) {
+
       setSubmitButtonState(submit, true);
     } else {
       setSubmitButtonState(submit, false);
-  
+
     }
   }
-  
-   checkForm() {
-   
-    this.setEventListeners('input', (evt) => handlerInputForm(evt,this.submit))
-    
-    
+
+  checkForm() {
+
+    this.setEventListeners('input', () => handlerInputForm(this.submit))
+
   }
 }
+
+
+
+
+
 
